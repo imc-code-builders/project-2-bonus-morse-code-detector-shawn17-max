b8ad91b07ac37adc62987cee0e3a232e988f0a26
Taishawn <roytaishawn17@icloud.com>
2026-02-10 18:24:26 -0600

    finished him

--- microbit_starter.py
+++ microbit_starter.py
@@ -1,6 +1,7 @@
 from microbit import *
 import music
 
+# Morse code dictionary
 morse_dict = {
     'dot-dash': 'A', 'dash-dot-dot-dot': 'B', 'dash-dot-dash-dot': 'C',
     'dash-dot-dot': 'D', 'dot': 'E', 'dot-dot-dash-dot': 'F',
@@ -22,21 +23,47 @@ current_symbol = ''
 message = []
 
 while True:
+    # Clear screen to make it responsive
     display.clear()
 
     # TASK 4: Shake to scroll the full message
-
+    if accelerometer.was_gesture('shake'):
+        if message:
+            display.scroll("".join(message))
+        else:
+            display.show("?")
+        message = [] # Clear message after scrolling
+        current_symbol = ''
 
     # TASK 2: Decode current_symbol when logo is touched
-    if pin_logo.is_touched():
-        print(current_symbol)
+    elif pin_logo.is_touched():
+        if current_symbol in morse_dict:
+            letter = morse_dict[current_symbol]
+            message.append(letter)
+            display.show(letter)
+            music.pitch(880, 100) # Higher tone for decoded letter
+            sleep(500)
+        else:
+            display.show(Image.NO) # Show X if invalid
+            sleep(500)
+        current_symbol = '' # Reset symbol after decoding
 
     # TASK 1: Record dots and dashes (Button A = dot, Button B = dash)
-    # Build current_symbol as a string like 'dot-dash-dot'
+    # TASK 3: Add display and sound
     elif button_a.was_pressed():
-        pass
+        if current_symbol == '':
+            current_symbol = 'dot'
+        else:
+            current_symbol += '-dot'
+        display.show('.')
+        music.pitch(440, 100) # Dit sound
+        sleep(200)
 
     elif button_b.was_pressed():
-        pass
-
-    # TASK 3: Add display and sound to each of the above
+        if current_symbol == '':
+            current_symbol = 'dash'
+        else:
+            current_symbol += '-dash'
+        display.show('-')
+        music.pitch(440, 300) # Dah sound
+        sleep(200)
