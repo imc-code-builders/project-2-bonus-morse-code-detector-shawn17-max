5d69e13a4b804db2c69fcb91e84ac4d7f3ad11b1
Taishawn <roytaishawn17@icloud.com>
2026-02-17 18:24:14 -0600

    Wemby the goat

--- microbit_starter.py
+++ microbit_starter.py
@@ -1,31 +1,25 @@
 from microbit import *
 import music
 
-# Morse code dictionary
+# Corrected Morse code dictionary using . and - instead of 'dot' and 'dash'
 morse_dict = {
-    'dot-dash': 'A', 'dash-dot-dot-dot': 'B', 'dash-dot-dash-dot': 'C',
-    'dash-dot-dot': 'D', 'dot': 'E', 'dot-dot-dash-dot': 'F',
-    'dash-dash-dot': 'G', 'dot-dot-dot-dot': 'H', 'dot-dot': 'I',
-    'dot-dash-dash-dash': 'J', 'dash-dot-dash': 'K', 'dot-dash-dot-dot': 'L',
-    'dash-dash': 'M', 'dash-dot': 'N', 'dash-dash-dash': 'O',
-    'dot-dash-dash-dot': 'P', 'dash-dash-dot-dash': 'Q', 'dot-dash-dot': 'R',
-    'dot-dot-dot': 'S', 'dash': 'T', 'dot-dot-dash': 'U',
-    'dot-dot-dot-dash': 'V', 'dot-dash-dash': 'W', 'dash-dot-dot-dash': 'X',
-    'dash-dot-dash-dash': 'Y', 'dash-dash-dot-dot': 'Z',
-    'dot-dash-dash-dash-dash': '1', 'dot-dot-dash-dash-dash': '2',
-    'dot-dot-dot-dash-dash': '3', 'dot-dot-dot-dot-dash': '4',
-    'dot-dot-dot-dot-dot': '5', 'dash-dot-dot-dot-dot': '6',
-    'dash-dash-dot-dot-dot': '7', 'dash-dash-dash-dot-dot': '8',
-    'dash-dash-dash-dash-dot': '9', 'dash-dash-dash-dash-dash': '0'
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+    '-----': '0'
 }
 
 current_symbol = ''
 message = []
 
 while True:
-    # Clear screen to make it responsive
+    # Clear screen briefly to make input responsive
     display.clear()
-
+    
     # TASK 4: Shake to scroll the full message
     if accelerometer.was_gesture('shake'):
         if message:
@@ -52,18 +46,18 @@ while True:
     # TASK 3: Add display and sound
     elif button_a.was_pressed():
         if current_symbol == '':
-            current_symbol = 'dot'
+            current_symbol = '.'
         else:
-            current_symbol += '-dot'
+            current_symbol += '.'
         display.show('.')
         music.pitch(440, 100) # Dit sound
         sleep(200)
-
+        
     elif button_b.was_pressed():
         if current_symbol == '':
-            current_symbol = 'dash'
+            current_symbol = '-'
         else:
-            current_symbol += '-dash'
+            current_symbol += '-'
         display.show('-')
         music.pitch(440, 300) # Dah sound
         sleep(200)
